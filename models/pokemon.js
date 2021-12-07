a4923e883f3eafeab9f8cff1d962d5a50ce1690b
n57r <grntboi90@gmail.com>
2021-12-07 08:56:05 -0800

    Third

--- models/pokemon.js
+++ models/pokemon.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Pokemon',
-    tableName: 'PokemonTable',
+    tableName: 'pokemontable',
     timestamps:false
   });
   return Pokemon;
